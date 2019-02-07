72aad5e944b5d10b6bebe3f572b04a42056b4ccc
Marco Antonio Anastacio Cintra <anastaciocintra@gmail.com>
2019-02-07 12:35:01 -0200

    Create make.sh

---
+++ src/make.sh
@@ -0,0 +1,19 @@
+rm -rf ~/tmp/escposcoffee
+mkdir ~/tmp/escposcoffee
+mkdir ~/tmp/escposcoffee/javadoc
+mkdir ~/tmp/escposcoffee/samples
+mkdir ~/tmp/escposcoffee/samples/lib
+cd escpos_coffee
+ant clean; ant jar; ant javadoc
+cp -R dist/javadoc ../../
+cp -R dist/javadoc /Users/marco/tmp/escposcoffee/
+cp dist/escpos_coffee.jar ~/tmp/escposcoffee/samples/lib
+cp dist/escpos_coffee.jar ~/tmp/escposcoffee
+cd ../samples/barcode/;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
+cd ../bitimage/;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
+cd ../charcode;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
+cd ../dithering;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
+cd ../getstart;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
+cd ../graphicsImage;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
+cd ../rasterimage;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
+cd ../textstyle;ant clean;ant jar;cp dist/*.jar ~/tmp/escposcoffee/samples
