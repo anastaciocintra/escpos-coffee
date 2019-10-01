091623c44eeb6406a9379710ef8967548b28437d
Marco Antonio Anastacio Cintra <anastaciocintra@gmail.com>
2019-10-01 17:35:46 -0300

    bug. LF char need #17 feed correction

--- escpos-coffee/src/main/java/com/github/anastaciocintra/escpos/EscPos.java
+++ escpos-coffee/src/main/java/com/github/anastaciocintra/escpos/EscPos.java
@@ -399,11 +399,15 @@ public class EscPos implements Closeable, Flushable, EscPosConst {
         if (nLines < 1 || nLines > 255) {
             throw new IllegalArgumentException("nLines must be between 1 and 255");
         }
-        byte[] configBytes = style.getConfigBytes();
-        write(configBytes, 0, configBytes.length);
-        write(ESC);
-        write('d');
-        write(nLines);
+        if(nLines == 1){
+            write(LF);
+        }else {
+            byte[] configBytes = style.getConfigBytes();
+            write(configBytes, 0, configBytes.length);
+            write(ESC);
+            write('d');
+            write(nLines);
+        }
         return this;
 
     }
