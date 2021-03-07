fc1679e885c38845852915ce2421e98006964d99
Marco Antonio Anastacio Cintra <anastaciocintra@gmail.com>
2021-03-07 10:44:29 -0300

    bug fix - adjusts for set charsetname for WPC1252 #62

--- .version/changeversion.sh
+++ .version/changeversion.sh
@@ -1,5 +1,5 @@
-old="4.0.0"
-new="4.0.1"
+old="4.0.2-SNAPSHOT"
+new="4.0.2"
 
 find ../ -type f \( -name "pom.xml" \) \
   -exec sed -i -e "s/<revision>$old<\/revision>/<revision>$new<\/revision>/g" {} \;
